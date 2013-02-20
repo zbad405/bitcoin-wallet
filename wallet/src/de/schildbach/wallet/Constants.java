47e00f04895f00cbd33a6273efaa43f3ff57f59d
Andreas Schildbach <andreas@schildbach.de>
2013-02-20 14:09:11 +0100

    blackberry: Remove references to Google Play

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -94,9 +94,6 @@ public class Constants
 	public static final String CREDITS_ICON_URL = "https://bitcointalk.org/index.php?action=profile;u=2062";
 	public static final String AUTHOR_TWITTER_URL = "https://twitter.com/#!/bitcoin_wallet";
 	public static final String AUTHOR_GOOGLEPLUS_URL = "https://profiles.google.com/andreas.schildbach";
-	public static final String MARKET_APP_URL = "market://details?id=%s";
-	public static final String WEBMARKET_APP_URL = "https://play.google.com/store/apps/details?id=%s";
-	public static final String MARKET_PUBLISHER_URL = "market://search?q=pub:\"Andreas Schildbach\"";
 
 	public static final String VERSION_URL = "http://wallet.schildbach.de/version";
 	public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
