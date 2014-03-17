bcfe1e088c757881909e2b578170cf783a45d947
Derf Shaya <derfshaya@gmail.com>
2014-03-18 01:49:23 +0800

    Chnged author information to avoid disturbing schildbach
    Changed some author information in About so as not to misdirect Zetacoin wallet user enquiries to Andreas Schildbach. Left Google Plus links intact; they still link to Andreas and the Bitcoin-wallet community. ZBAD doesn't have a presence on GPlus that I know of.

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -82,15 +82,15 @@ public class Constants
 	public static final int MBTC_MAX_PRECISION = 5;
 	public static final int LOCAL_PRECISION = 4;
 
-	public static final String DONATION_ADDRESS = "18CK5k1gajRKKSC7yVSTXT9LUzbheh1XY4";
-	public static final String REPORT_EMAIL = "wallet@schildbach.de";
+	public static final String DONATION_ADDRESS = "ZYyhXSdrsGdZe9guGnZ3TbUi9cywrz2aZj";
+	public static final String REPORT_EMAIL = "derfshaya@gmail.com";
 	public static final String REPORT_SUBJECT_ISSUE = "Reported issue";
 	public static final String REPORT_SUBJECT_CRASH = "Crash report";
 
 	public static final String LICENSE_URL = "http://www.gnu.org/licenses/gpl-3.0.txt";
-	public static final String SOURCE_URL = "https://github.com/schildbach/bitcoin-wallet";
-	public static final String BINARY_URL = "http://code.google.com/p/bitcoin-wallet/downloads/list";
-	public static final String CREDITS_BITCOINJ_URL = "http://code.google.com/p/bitcoinj/";
+	public static final String SOURCE_URL = "https://github.com/zbad405/zetacoin-wallet";
+	public static final String BINARY_URL = "https://github.com/zbad405/zetacoin-wallet";
+	public static final String CREDITS_ZETACOINJ_URL = "https://github.com/zbad405/zetacoinj";
 	public static final String CREDITS_ZXING_URL = "http://code.google.com/p/zxing/";
 	public static final String CREDITS_ICON_URL = "https://bitcointalk.org/index.php?action=profile;u=2062";
 	public static final String AUTHOR_TWITTER_URL = "https://twitter.com/#!/bitcoin_wallet";
@@ -98,7 +98,7 @@ public class Constants
 	public static final String COMMUNITY_GOOGLEPLUS_URL = "https://plus.google.com/communities/105515929887248493912";
 	public static final String MARKET_APP_URL = "market://details?id=%s";
 	public static final String WEBMARKET_APP_URL = "https://play.google.com/store/apps/details?id=%s";
-	public static final String MARKET_PUBLISHER_URL = "market://search?q=pub:\"Andreas Schildbach\"";
+	public static final String MARKET_PUBLISHER_URL = "market://search?q=pub:\"ZBAD305\"";
 
 	public static final String VERSION_URL = "http://wallet.schildbach.de/version";
 	public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
